8e0678d7a9db3a0cb92d1be065cc517de947eea3
Teragron <54573938+Teragron@users.noreply.github.com>
2021-09-08 23:17:40 +0200

    Update TrainedAgent.py

--- TrainedAgent.py
+++ TrainedAgent.py
@@ -2,81 +2,47 @@ import random
 import time
 
 from utils.getkeys import key_check
-import pydirectinput
 import keyboard
 import time
 import cv2
 from utils.grabscreen import grab_screen
 from utils.directkeys import PressKey, ReleaseKey, W, D, A
-from fastai.vision.all import *
+from fastai.vision.all import *                 
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
 def label_func(x): return x.parent.name
-learn_inf = load_learner("C:/Users/programmer/Desktop/FallGuys/GC.pkl")
+learn_inf = load_learner("Models/178k.pkl")
 print("loaded learner")
 
-# Sleep time after actions
+
 sleepy = 0.1
 
-# Wait for me to push B to start.
 keyboard.wait('B')
 time.sleep(sleepy)
 
-# Hold down W no matter what!
-keyboard.press('w')
-
-# Randomly pick action then sleep.
-# 0 do nothing release everything ( except W )
-# 1 hold left
-# 2 hold right
-# 3 Press Jump
 
 while True:
 
-    image = grab_screen(region=(50, 100, 799, 449))
+    image = grab_screen(region=(50, 130, 500, 520))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.Canny(image, threshold1=200, threshold2=300)
-    image = cv2.resize(image,(224,224))
-    # cv2.imshow("Fall", image)
-    # cv2.waitKey(1)
+    image = cv2.Canny(image, threshold1=200, threshold2=400)
+    image = cv2.resize(image,(84,84))
+
     start_time = time.time()
     result = learn_inf.predict(image)
     action = result[0]
-    #print(result[2][0].item(), result[2][1].item(), result[2][2].item(), result[2][3].item())
 
-    #action = random.randint(0,3)
     
-    if action == "Jump" or result[2][0]>.1:
-        print(f"JUMP! - {result[1]}")
+    if action == "Space":
         keyboard.press("space")
-        keyboard.release("a")
-        keyboard.release("d")
-        time.sleep(sleepy)
+        print(action)
 
-    if action == "Nothing":
-        #print("Doing nothing....")
-        keyboard.release("a")
-        keyboard.release("d")
+    elif action == "Nothing":
         keyboard.release("space")
-        time.sleep(sleepy)
+        print(action)
 
-    elif action == "Left":
-        print(f"LEFT! - {result[1]}")
-        keyboard.press("a")
-        keyboard.release("d")
-        keyboard.release("space")
-        time.sleep(sleepy)
-
-    elif action == "Right":
-        print(f"Right! - {result[1]}")
-        keyboard.press("d")
-        keyboard.release("a")
-        keyboard.release("space")
-        time.sleep(sleepy)
-
-
-    # End simulation by hitting h
     keys = key_check()
     if keys == "H":
         break
-
-keyboard.release('W')
